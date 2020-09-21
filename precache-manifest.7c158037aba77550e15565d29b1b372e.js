64c8b9d78af8bc2adc8bf166bd1838158c96beec
kathya marquez <knmarquez25@gmail.com>
2020-09-21 00:28:35 -0700

    Updates

---
+++ precache-manifest.7c158037aba77550e15565d29b1b372e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a18fee31be00820c5d9562b8f4a7218c",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "1938765632fd654c02de",
+    "url": "/my-app/static/css/main.f7dd9788.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/my-app/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1938765632fd654c02de",
+    "url": "/my-app/static/js/main.9dea2abc.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
